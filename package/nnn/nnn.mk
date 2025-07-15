c0b2061bec1e41b19fd1f7bd655d5fbd17e9c337
Alexander Shirokov <shirokovalexs@gmail.com>
2025-07-15 13:47:16 +0200

    package/nnn: new package
    nnn is a fast and minimal text-based file manager. It is written in C
    and has minimal dependencies, which makes it portable and suitable for
    embedded systems.
    
    Project page: https://github.com/jarun/nnn
    Wiki: https://en.wikipedia.org/wiki/Nnn_(file_manager)
    
    Signed-off-by: Alexander Shirokov <shirokovalexs@gmail.com>

---
+++ package/nnn/nnn.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# nnn
+#
+################################################################################
+
+NNN_VERSION = v5.1
+NNN_LICENSE = BSD-2-Clause
+NNN_LICENSE_FILES = LICENSE
+NNN_SITE = $(call github,jarun,nnn,$(NNN_VERSION))
+NNN_DEPENDENCIES = readline ncurses
+NNN_INSTALL_STAGING = YES
+NNN_BIN = usr/bin/nnn
+
+define NNN_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+	CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
+endef
+
+# Install the binaries, manuals, and other items described in the Makefile to the staging directory
+define NNN_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+	DESTDIR=$(STAGING_DIR) PREFIX=/usr install
+endef
+
+# Put a stripped binary into the target directory
+define NNN_INSTALL_TARGET_CMDS
+	$(INSTALL) -Dm755 $(STAGING_DIR)/$(NNN_BIN) $(TARGET_DIR)/$(NNN_BIN)
+	$(TARGET_STRIP) $(TARGET_DIR)/$(NNN_BIN)
+endef
+
+$(eval $(generic-package))
