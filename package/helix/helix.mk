af430f69019156468bd62819a1a0af35e44003dc
Alexander Shirokov <shirokovalexs@gmail.com>
2025-07-16 18:04:49 +0200

    helix:add host-pkgconfig

--- package/helix/helix.mk
+++ package/helix/helix.mk
@@ -8,5 +8,6 @@ HELIX_VERSION = 25.01.1
 HELIX_SITE = $(call github,helix-editor,helix,$(HELIX_VERSION))
 HELIX_LICENSE = MPL-2.0
 HELIX_LICENSE_FILES = LICENSE
+HELIX_DEPENDENCIES = host-pkgconf
 
 $(eval $(cargo-package))
