89570fb6c9b64825f1b8a0344581ef0a6c555309
Alexander Shirokov <shirokovalexs@gmail.com>
2025-07-16 18:10:13 +0200

    helix:use custom git source

--- package/helix/helix.mk
+++ package/helix/helix.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-HELIX_VERSION = 25.01.1
-HELIX_SITE = $(call github,helix-editor,helix,$(HELIX_VERSION))
+HELIX_VERSION = e7ac2fcdecfdbf43a4f772e7f7c163b43b3d0b9b
+HELIX_SITE = https://github.com/alexs-sh/helix.git
+HELIX_SITE_METHOD = git
 HELIX_LICENSE = MPL-2.0
 HELIX_LICENSE_FILES = LICENSE
 HELIX_DEPENDENCIES = host-pkgconf
