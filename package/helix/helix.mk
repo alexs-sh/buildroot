28db8dae0390fb051cbbd919e223ea2ec7667fa2
Alexander Shirokov <shirokovalexs@gmail.com>
2025-07-22 13:05:42 +0200

    helix:bump version

--- package/helix/helix.mk
+++ package/helix/helix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HELIX_VERSION = 25.01.1
+HELIX_VERSION = 25.07.1
 HELIX_SITE = $(call github,helix-editor,helix,$(HELIX_VERSION))
 HELIX_LICENSE = MPL-2.0
 HELIX_LICENSE_FILES = LICENSE
