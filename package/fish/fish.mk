f923411a4a0e7db23ae94892711262decc4d1e10
Alexander Shirokov <shirokovalexs@gmail.com>
2025-08-23 19:06:00 +0200

    fish:use git

--- package/fish/fish.mk
+++ package/fish/fish.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-FISH_VERSION = 4.0.2
-FISH_SITE = $(call github,fish-shell,fish-shell,$(FISH_VERSION))
+FISH_VERSION = 67e86571097e6ac35a637344064aa6445f07da03
+FISH_SITE = https://github.com/fish-shell/fish-shell.git
+FISH_SITE_METHOD = git
 FISH_LICENSE = GPL-2.0
 FISH_LICENSE_FILES = LICENSE
 FISH_CARGO_ENV=FISH_BUILD_VERSION=$(FISH_VERSION) FISH_BUILD_DOCS=0
