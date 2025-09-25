959d79ba1cd0965f4fe3b84708e4bea05fd79a6b
Alexander Shirokov <shirokovalexs@gmail.com>
2025-09-25 12:15:12 +0200

    remove PROJECT_NAME_FOR_VERSION_STRING

--- package/uutils-coreutils/uutils-coreutils.mk
+++ package/uutils-coreutils/uutils-coreutils.mk
@@ -8,7 +8,6 @@ UUTILS_COREUTILS_VERSION = 0.2.2
 UUTILS_COREUTILS_SITE = $(call github,uutils,coreutils,$(UUTILS_COREUTILS_VERSION))
 UUTILS_COREUTILS_LICENSE = MIT
 UUTILS_COREUTILS_LICENSE_FILES = LICENSE
-UUTILS_COREUTILS_CARGO_ENV=PROJECT_NAME_FOR_VERSION_STRING="uutils coreutils"
 
 # The list of default utilities is used when no specific instructions are provided
 # from the configuration.
